e75b8347afbea806c5bf53a4b55782526d5791bf
Vũ. Vy Quốc <vyquocvu@gmail.com>
2022-08-02 15:29:09 +0700

    add test

--- test/test.js
+++ test/test.js
@@ -1,25 +1,27 @@
 
-const anyState = require('./anyState');
+const anyState = require('../src/anyState');
 const { createAnyState, getState, setItem, getItem, watch } = anyState;
 
 function main () {
   createAnyState({
     name: 'John',
     age: 30,
-    isMarried: false,
-    friends: ['Mary', 'Bob'],
+    children: [{
+      name: 'Bob',
+      age: 5,
+    }]
   });
-  console.log('First State', getState());
+  const state = getState();
+  console.log('First State', state);
+  state.name = 'Jane';
+  console.log('Not State', state);
+  console.log('Update State', getState());
   watch('name', (newName, oldName) => {
     console.log(`${oldName} is now ${newName}`);
     console.log('Next State', getState());
   })
 
   setItem('name', 'Jane');
-  setItem('age', 31);
-  setItem('isMarried', true);
-  console.log(getItem('name')); // John
-  console.log(getItem('age')); // 31
-  console.log(getItem('isMarried')); // true
+  console.log('children', getItem('children.0'));
 }
 main();
